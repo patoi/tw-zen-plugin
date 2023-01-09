3cc2921e097abce2f8c6c4d1369447f45d06b7c9
István Pató <istvan.pato@gmail.com>
2023-01-09 15:50:37 +0100

    style: formatting

--- plugin.cjs
+++ plugin.cjs
@@ -6,13 +6,16 @@ module.exports = plugin(({ addBase }) => {
 			'@apply delay-300 duration-[3000ms] opacity-0 transition-all transform-gpu': {}
 		},
 		'.zen-pop-up': {
-			'@apply delay-500 duration-[1800ms] origin-bottom opacity-0 translate-y-8 transition-all transform-gpu': {}
+			'@apply delay-500 duration-[1800ms] origin-bottom opacity-0 translate-y-8 transition-all transform-gpu':
+				{}
 		},
 		'.zen-from-left': {
-			'@apply delay-700 duration-[1800ms] opacity-0 -translate-x-1/3 transition-all transform-gpu': {}
+			'@apply delay-700 duration-[1800ms] opacity-0 -translate-x-1/3 transition-all transform-gpu':
+				{}
 		},
 		'.zen-from-right': {
-			'@apply delay-700 duration-[1800ms] opacity-0 translate-x-1/3 transition-all transform-gpu': {}
+			'@apply delay-700 duration-[1800ms] opacity-0 translate-x-1/3 transition-all transform-gpu':
+				{}
 		},
 		'.zen-spin-forever': {
 			'@apply animate-[spin_30s_linear_infinite]': {}
